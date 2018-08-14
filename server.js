d7558766e9ea67c1aed3ee7a8870bd93139d54fe
Roman Jin <romanjin2017@outlook.com>
2018-08-15 02:00:22 +0800

    Simple crud

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express 	= require('express'),
+	path 		= require('path'),
+	bodyParser 	= require('body-parser'),
 	OrientDB 	= require('orientjs'),
 	config		= require('./config.json'),
 	app 		= express();
 
-app.use(express.static('src'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public/views'));
+app.set('view engine', 'pug');
 
 // Connect db server
 var server = OrientDB({
@@ -21,22 +30,74 @@ db = server.use({
 	password: config.db.password
 });
 
-async function create_test() {
-	
-	// db.class.list().then((classes) => {
-	// 	classes.forEach((class_) => {
-	// 		console.log(class_.name)
-	// 	})
-	// })
+// Index
+app.get('/', (req, res) => {
+	Promise.all([get_tasks()])
+	.then(function(response) {
+		res.render('index', {tasks: response[0]})
+	})
+	.catch(function(e){ console.log(e); });
+});
 
-	// let P = db.class.create('Todo').then((todo) => {
-	// 	console.log('Created Class: ' + todo.name)
-	// });
+// Create page
+app.get('/create', (req, res) => {
+	res.render('create');
+})
+
+// Store task
+app.post('/store', (req, res) => {
+	Promise.all([insert_task(req.body)])
+	.then(function() {
+		res.redirect('/')
+	})
+	.catch(function(e){ console.log(e); });
+})
+
+// Edit page
+app.get('/edit/:cluster/:position', (req, res) => {
+	db.record.get(`#${req.params.cluster}:${req.params.position}`)
+	.then((record) => {
+		res.render('edit', {task: record});
+	})
+	.catch((e) => {
+		console.log(e)
+	})
+})
+
+// Update task
+app.post('/update', (req, res) => {
+	db.record.get(`#${req.body.cluster}:${req.body.position}`)
+	.then((record) => {
+		record.name = req.body.name
+		db.record.update(record)
+			.then(function() {
+				res.redirect('/')
+			})
+		// res.render('edit', {task: record});
+	})
+})
+
+app.get('/delete/:cluster/:position', (req, res) => {
+	db.record.delete(`#${req.params.cluster}:${req.params.position}`)
+	.then((record) => {
+		res.redirect('/')
+	})
+	.catch((e) => {
+		console.log(e)
+	})
+})
+
+async function insert_task(request) {
+	var task = await db.class.get('Task')
+	return task.create({
+		name: request.name
+	})
 }
 
-Promise.all([create_test()])
-.then(function() { console.log('all done'); server.close(); })
-.catch(function(e){ console.log(e); server.close(); });
+async function get_tasks() {
+	var task = await db.class.get('Task')
+	return await task.list()
+}
 
 app.set('port', process.env.PORT || 7000)
 app.listen(app.get('port'), function () {
